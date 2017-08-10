0338a3d3a9f4f3ddfcc4dcdc787c47db8b258998
Mark Helt and Riki Montgomery <mark@>
2017-08-10 11:44:57 -0700

    Add form that toggles diffferent height options and displays rides.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-
+  $('#userHeight').change(function(event){
+    var height = parseInt($("#userHeight").val());
+    if (height === 1) {
+      $(".shortHeight").show();
+      $(".mediumHeight, .tallHeight").hide();
+    }
+    if (height === 2) {
+      $(".shortHeight, .mediumHeight").show();
+      $(".tallHeight").hide();
+    }
+    if (height === 3) {
+      $(".shortHeight, .mediumHeight, .tallHeight").show();
+    }
+    // // if (height === 'height2') {
+    // //   $(".shortHeight mediumHeight").show();
+    // }
+    event.preventDefault();
+  });
 });
